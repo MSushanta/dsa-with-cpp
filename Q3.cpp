416c2edc4c32f00f2937037894551970764f8a1d
MSushanta <158486769+MSushanta@users.noreply.github.com>
2024-02-26 12:40:39 +0530

    solution of Assignment 1

---
+++ Q3.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Space requerd for Int datatype"<<endl;
+    cout<<sizeof(int);   
+    cout<<endl<<"Space requerd for bool datatype"<<endl;
+    cout<<sizeof(bool);   
+    cout<<endl<<"Space requerd for float datatype"<<endl;
+    cout<<sizeof(float);   
+}
---
+++ Q5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float r = 10,s;
+   s = 2 * 3.141 * r;
+   cout<<"The sircumtances of the circle is: "<<s<<endl;  
+}
